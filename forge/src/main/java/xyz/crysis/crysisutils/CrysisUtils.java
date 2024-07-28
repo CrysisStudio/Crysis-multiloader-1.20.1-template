a211eb082fbc39a3de29268d4611b4f98a7d5a56
histerdev <jistahj@gmail.com>
2024-07-28 12:32:49 -0400

    init

---
+++ forge/src/main/java/xyz/crysis/crysisutils/CrysisUtils.java
@@ -0,0 +1,19 @@
+package xyz.crysis.crysisutils;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(Constants.MOD_ID)
+public class CrysisUtils {
+    
+    public CrysisUtils() {
+    
+        // This method is invoked by the Forge mod loader when it is ready
+        // to load your mod. You can access Forge and Common code in this
+        // project.
+    
+        // Use Forge to bootstrap the Common mod.
+        Constants.LOG.info("Hello Forge world!");
+        CommonClass.init();
+        
+    }
+}
